1197631bcd0e72f1e14eb7e557ac77ac80fcaaaa
TylerKang-97 <kingksy777@gmail.com>
2021-08-09 16:08:05 +0900

    AllMovies_Page카운팅_0809

--- src/routes/AllMovies.js
+++ src/routes/AllMovies.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import { withStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { Link } from 'react-router-dom';
@@ -21,63 +26,79 @@ class AllMovies extends React.Component {
             movies: [],
             keyword: '',
             tmdbs: [],
-            count: 1,
+            count: 0,
         };
         this.handleChange = this.handleChange.bind(this); // 바인딩
-        this.onClickHander = this.onClickHander.bind(this); // 바인딩
+        this.onNextClick = this.onNextClick.bind(this); // 바인딩
+        this.onBeforeClick = this.onBeforeClick.bind(this);
     }
 
     handleChange = (e) => {
-        this.setState({ keyword: e.target.value });
-    };
-    onClickHander = () => {
-        this.setState({ count: this.state.count + 1 });
-        console.log('count', this.state.count);
-    };
-    getTmdbMoives = async () => {
-        // Tmdb API 이용
+        this.setState({keyword: e.target.value});  
+    }
+
+    onNextClick = () => {
+        this.setState({count: this.state.count+1});
+        this.getTmdbMoives();
+    }
+    onBeforeClick = () => {
+        if(this.state.count >= 2) {
+            this.setState({count: this.state.count-1});
+            this.getTmdbMoives();
+        }else {
+            alert('처음페이지입니다.');
+        }
+    }
+
+     getTmdbMoives = async () => { // Tmdb API 이용
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const COUNT = this.state.count;
         const {
-            data: { results },
+            data: {
+                results,
+            },
         } = await axios.get(`
-        https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=ko&page=${COUNT}&region=KR`);
+        https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=ko&page=${COUNT+1}&region=KR`);
         console.log('tmdb', results);
-        this.setState({ tmdbs: results, isLoading: false });
+        console.log('COUNT', COUNT);
+        this.setState({tmdbs:results, isLoading: false});
     };
-
+    
     componentDidMount() {
-        // this.getAllMovies();
         this.getTmdbMoives();
     }
-
+    
     render() {
-        const { isLoading, tmdbs } = this.state;
-        let url = '/viewMovie?movieNm=';
+        const { isLoading, tmdbs, count } = this.state;
+        let url = '/viewMovie?movieNm='; 
         return (
             <>
-                <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} placeholder="검색" />
-                <button onClick={this.onClickHander}>다음페이지</button>
+            <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} placeholder="검색" />
+            <button onClick={this.onBeforeClick}>이전페이지</button>
+            <button onClick={this.onNextClick} >다음페이지</button>  
+            <p>현재 페이지:{this.state.count}</p>
                 {isLoading ? (
                     <TableHead>'영화 목록을 불러오는 중.'</TableHead>
-                ) : (
+                ) : (  
                     tmdbs.map((m) => (
                         <>
-                            {m.original_title.indexOf(this.state.keyword) > -1 ? (
-                                <TableRow>
-                                    <TableCell>
-                                        <Link to={url + m.original_title}>
-                                            {m.original_title}({m.title})
-                                        </Link>
-                                    </TableCell>
-                                    <TableCell>{m.release_date}</TableCell>
-                                    <TableCell>{m.vote_average}점</TableCell>
-                                </TableRow>
-                            ) : (
-                                <p></p>
-                            )}
+                         {m.original_title.indexOf(this.state.keyword) > -1 ?
+                        <TableRow>
+                        <TableCell>
+                        <Link to={url + m.original_title}>
+                        {m.original_title}({m.title})
+                        </Link>
+                        </TableCell>
+                        <TableCell>
+                        {m.release_date}
+                        </TableCell>
+                        <TableCell>
+                        {m.vote_average}점
+                        </TableCell>
+                        </TableRow>
+                        : <p></p> }
                         </>
-                    ))
+                    ))            
                 )}
             </>
         );
