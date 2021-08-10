6133b66fdc34b2dfc0dd5960bb4ab3cbc3eb559b
TylerKang-97 <kingksy777@gmail.com>
2021-08-10 20:02:48 +0900

    tmdbList_Dialog구현중._0810

--- src/routes/TmdbList.js
+++ src/routes/TmdbList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import axios from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 
 class TmdbList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoading: true,
-            movies: [],
             tmdbs: [],
-            count: 1,
             posters: [],
+            titles: '',
+            movies: '',
+            open: false,
+            date:'',
+
         };
-        
+        this.onClickHandle = this.onClickHandle.bind(this);
+        this.onCloseHandle = this.onCloseHandle.bind(this);
     }
 
      getTrendingMovies = async () => { // Tmdb API 이용
@@ -28,15 +36,36 @@ class TmdbList extends React.Component {
         this.setState({tmdbs:results, isLoading: false});
     };
 
+    onClickHandle = (e) => {
+        this.setState({titles: e.target.title, movies: e.target.id, open: true});
+        console.log('titles', this.state.titles);
+        console.log('poster_path', this.state.movies);
+    }
+
+    onCloseHandle = () => {
+        this.setState({open: false})
+    }
+
     componentDidMount() {
         this.getTrendingMovies();
     }
     
     render() {
         const { tmdbs } = this.state;
+        let url = 'https://image.tmdb.org/t/p/w200';
         return (
         tmdbs.map((m) => (
-            <img src={`https://image.tmdb.org/t/p/w200/${m.poster_path}`} alt='img' />
+            <>
+            <img src={url + m.poster_path} alt='img' onClick={this.onClickHandle}
+             id={[m.id, m.release_date, m.poster_path]}
+             title={m.title}
+              />
+            <Dialog open={this.state.open} onClose={this.onCloseHandle}>
+                <DialogTitle>{this.state.titles}</DialogTitle>
+                <DialogContent>{/*this.state.movies*/}<br />
+                <img src={url + this.state.movies.substring(18,50)} /></DialogContent>
+            </Dialog>
+            </>
         ))
         );
     }
---
+++ src/routes/ViewTMDB.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const ViewTMDB = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useStaet([]);
+
+    const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY; 
+
+}
+export default ViewTMDB;
