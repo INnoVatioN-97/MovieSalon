c3a17b94d8c91e1ee1585a2a912c703cf53b7bbb
INnoVatioN-97 <koyoungil97@gmail.com>
2021-08-25 16:52:37 +0900

    주석제거

--- src/components/Cast.js
+++ src/components/Cast.js
@@ -4,31 +4,37 @@ import { TableCell, TableRow, Table } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 class Cast extends React.Component {
-constructor(props) {
-    super(props);
-    this.state = {
-        isLoading: false,
-        personId: props.id,
-        castInfo: [],
-        movies: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            personId: props.id,
+            castInfo: [],
+            movies: [],
+        };
     }
-}
 
-    getCastInfo = async (ID) => { // 인물 정보 api
+    getCastInfo = async (ID) => {
+        // 인물 정보 api
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const {
-            data: {id, name, gender, birthday, place_of_birth, profile_path},
-        } = await axios.get(`https://api.themoviedb.org/3/person/${ID}?api_key=${TMDB_API_KEY}&language=en-US`);
-        this.setState({castInfo: [id, name, gender, birthday, place_of_birth, profile_path ] });
-    }
+            data: { id, name, gender, birthday, place_of_birth, profile_path },
+        } = await axios.get(
+            `https://api.themoviedb.org/3/person/${ID}?api_key=${TMDB_API_KEY}&language=en-US`
+        );
+        this.setState({ castInfo: [id, name, gender, birthday, place_of_birth, profile_path] });
+    };
 
-    getFilmoMovies = async (ID) => { // 인물이 출연한 영화 api
+    getFilmoMovies = async (ID) => {
+        // 인물이 출연한 영화 api
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const {
             data: { cast },
-        } = await axios.get(`https://api.themoviedb.org/3/person/${ID}/movie_credits?api_key=${TMDB_API_KEY}&language=en-US`);
-        this.setState({movies: cast.slice(0,5)});
-    }
+        } = await axios.get(
+            `https://api.themoviedb.org/3/person/${ID}/movie_credits?api_key=${TMDB_API_KEY}&language=en-US`
+        );
+        this.setState({ movies: cast.slice(0, 5) });
+    };
 
     componentDidMount() {
         this.getCastInfo(this.state.personId);
@@ -39,28 +45,33 @@ constructor(props) {
         const { castInfo, movies } = this.state;
         const imgUrl = 'https://image.tmdb.org/t/p/w200';
         let queryUrl = '/viewTmdb/';
-        return(
+        return (
             <>
-            <TableRow>
-                <TableCell><img src={imgUrl + castInfo[5]} alt={castInfo[1]}/></TableCell>
+                <TableRow>
                     <TableCell>
-                        <TableRow><h2>{castInfo[1]}</h2></TableRow>
+                        <img src={imgUrl + castInfo[5]} alt={castInfo[1]} />
+                    </TableCell>
+                    <TableCell>
+                        <TableRow>
+                            <h2>{castInfo[1]}</h2>
+                        </TableRow>
                         <TableRow>{castInfo[3]}</TableRow>
-                        <TableRow>{castInfo[2] === 1 ? (<p>woman</p>) : (<p>man</p>)}</TableRow>
+                        <TableRow>{castInfo[2] === 1 ? <p>woman</p> : <p>man</p>}</TableRow>
                         <TableRow>{castInfo[4]}</TableRow>
                     </TableCell>
-            </TableRow>
-            <TableRow>
-                {movies.map((m) => (
-                    <TableCell><Link to={ queryUrl + m.id }><img src={imgUrl + m.poster_path}/></Link></TableCell>
-                ))}
-            </TableRow>
-            
-            
+                </TableRow>
+                <TableRow>
+                    {movies.map((m) => (
+                        <TableCell>
+                            <Link to={queryUrl + m.id}>
+                                <img src={imgUrl + m.poster_path} />
+                            </Link>
+                        </TableCell>
+                    ))}
+                </TableRow>
             </>
         );
     }
-
-    }
+}
 
 export default Cast;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TableCell, TableRow } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-const cheerio = require('cheerio');
 
 const styles = makeStyles({
     movieTitle: {
@@ -17,79 +15,7 @@ const styles = makeStyles({
 });
 
 const Movie = ({ movieCode, movieNm, openDt, rank, audiAcc, audiInten, rankInten }) => {
-    // const [movieCode, setMovieCode] = useState(0);
-    // const [movieLink, setMovieLink] = useState('');
-    // const [poster, setPoster] = useState('');
-
-    // const ID_KEY = process.env.REACT_APP_NAVER_CLIENT_ID;
-    // const SECRET_KEY = process.env.REACT_APP_NAVER_CLIENT_SECRET_KEY;
     console.log('movieCode from Movie:', movieCode);
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             if (movieNm === '') {
-    //                 setMovieCode(0);
-    //             } else {
-    //                 const {
-    //                     data: { items },
-    //                 } = await axios.get('/api/v1/search/movie.json', {
-    //                     params: { query: movieNm, display: 5 },
-    //                     headers: {
-    //                         'X-Naver-Client-Id': ID_KEY,
-    //                         'X-Naver-Client-Secret': SECRET_KEY,
-    //                     },
-    //                 });
-    //                 //주소를 통해 가져와 진 items 배열을 movies라는 state에 저장.
-    //                 console.log('items[0] from Movie.js:', items[0]);
-    //                 setMovieLink(items[0].link);
-    //             }
-    //         } catch (error) {
-    //             console.log('error 발생! ', error);
-    //         }
-    //     }
-    //     fetchData();
-    // }, []);
-
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         try {
-    //             // console.log('movieInfo (line65):', movieInfo);
-
-    //             if (movieLink !== null || movieLink !== undefined) {
-    //                 let tmp = movieLink;
-    //                 setMovieCode(tmp.split('?code=')[1]);
-    //             }
-
-    //             // console.log(`code from line 72: ${code}`); //정확히 전달 됨.
-    //             return await axios.get(
-    //                 '/poster/movie/bi/mi/photoViewPopup.naver?movieCode=' + movieCode
-    //             );
-    //             // console.log(res);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    //     fetchData()
-    //         .then((res) => {
-    //             let $ = cheerio.load(res.data);
-    //             // ul.list--posts를 찾고 그 children 노드를 bodyList에 저장
-    //             const bodyList = $('#page_content').children('a').children('#targetImage');
-    //             // hqPoster = bodyList[0].attribs.src;
-    //             // console.log('bodyList[0].attribs.src:', bodyList[0].attribs.src);
-    //             let hqPoster = bodyList[0].attribs.src;
-    //             if (hqPoster !== null || hqPoster !== undefined) {
-    //                 setPoster(bodyList[0].attribs.src);
-    //                 console.log('poster:', poster);
-    //                 console.log('movieCode:', movieCode);
-    //             }
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
-    // }, [movieLink, movieCode]);
-
-    //props에 저장된 classes(withStyles에 필요), 영화이름, 순위, 개봉일과 같은 정보들을 가져와 변수로 관리.
-    // const { classes, movieNm, openDt, rank, audiAcc, audiInten, rankInten } = this.props;
     const classes = styles();
     let url = '/viewMovie?movieNm=';
 
