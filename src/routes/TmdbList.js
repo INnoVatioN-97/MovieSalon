c0253d9128608b682141d6325a75a64f463469a3
INnoVatioN-97 <koyoungil97@gmail.com>
2021-09-02 17:59:33 +0900

    TMDB List 함수형 컴포넌트로 변환

--- src/routes/TmdbList.js
+++ src/routes/TmdbList.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-import { index } from 'cheerio/lib/api/traversing';
 import DefaultProfileImage from 'images/DefaultProfileImage.png';
 import NoImageAvailable from 'images/NoImageAvailable.png';
 import { Grid, Paper } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles';
 import ReactPlayer from 'react-player';
 import '../css/Dialog.css';
 
@@ -24,38 +23,30 @@ import '../css/Dialog.css';
  *
  * 캐스팅 목록 그리드형 배치 위해 9번 줄에 Dialog 전담 css 파일 생성 후 적용.
  */
- const styles = theme => ({
+const styles = makeStyles((theme) => ({
     root: {
-      padding: theme.spacing(3),
-      background: '#eeeeee'
+        padding: theme.spacing(3),
+        background: '#eeeeee',
     },
     margins: {
-        padding:'2,3,4,5',
+        padding: '2,3,4,5',
     },
-  });
+}));
 
+const TmdbList = ({ tmdbHome, upcomming }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [viewChange, setViewChange] = useState(false);
+    const [titles, setTitles] = useState('');
+    const [movies, setMovies] = useState('');
+    const [castMember, setCastMember] = useState([]);
+    const [trailers, setTrailers] = useState([]);
+    const [similars, setSimilars] = useState([]);
+    const [tmdbs, setTmdbs] = useState(tmdbHome);
+    const [upcommings, setUpcommings] = useState(upcomming);
 
-class TmdbList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            open: false,
-            viewChange: false,
-            titles: '',
-            movies: '',
-            castMember: [],
-            trailers: [],
-            similer: [],
-            tmdbs: props.tmdbHome,
-            upcommings: props.upcomming,
-        };
-        this.onOpenChange = this.onOpenChange.bind(this);
-        this.onCloseHandle = this.onCloseHandle.bind(this);
-        this.onClickHandles = this.onClickHandles.bind(this);
-    }
-
-    getMovieCasts = async (id) => { // 출연진 정보
+    const getMovieCasts = async (id) => {
+        // 출연진 정보
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const {
             data: { cast },
@@ -63,69 +54,72 @@ class TmdbList extends React.Component {
             `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${TMDB_API_KEY}`
         );
         console.log('cast', cast);
-        this.setState({ castMember: cast.slice(0, 5), isLoading: false }); // 출연진 5명만 추출(slice())
+        // this.setState({ castMember: cast.slice(0, 5), isLoading: false }); // 출연진 5명만 추출(slice())
+        setCastMember(cast.slice(0, 5));
+        setIsLoading(false);
     };
 
-    getMovieVideos = async (id) => { // 트레일러
+    const getMovieVideos = async (id) => {
+        // 트레일러
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const {
             data: { results },
         } = await axios.get(`
         https://api.themoviedb.org/3/movie/${id}/videos?api_key=${TMDB_API_KEY}&language=en-US`);
         console.log('videos', results);
-        this.setState({ trailers: results.slice(0, 1) });
-        console.log('videos_slice', this.state.trailers);
+        // this.setState({ trailers: results.slice(0, 1) });
+        setTrailers(results.slice(0, 1));
+        // console.log('videos_slice', this.state.trailers);
     };
 
-    getSimilerMovies = async (id) => { // 유사한 영화
+    const getSimilerMovies = async (id) => {
+        // 유사한 영화
         const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
         const {
             data: { results },
         } = await axios.get(
             `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${TMDB_API_KEY}&language=ko&page=1`
         );
-        this.setState({ similer: results.slice(0, 4) });
+        // this.setState({ similer: results.slice(0, 4) });
+        setSimilars(results.slice(0, 4));
     };
 
     // 영화 포스터를 클릭하면 다이얼로그를 띄우도록 하는 함수
-    onOpenChange = (e) => {
-        // Click발생한 영화포스터의 제목, 영화정보(,로 split), 다이얼로그 상태변경
-        this.setState({
-            open: !this.state.open,
-            titles: e.target.title,
-            movies: e.target.id.split(','),
-        });
-        console.log('titles', this.state.titles);
+    const onOpenChange = (e) => {
+        setOpen(!open);
+        setTitles(e.target.title);
+        setMovies(e.target.id.split(','));
         console.log('target값', e.target.id.substring(0, 7));
-        console.log('movies_posterClick', this.state.movies); // 클릭된 포스터의 영화정보 가져옴
-        this.getMovieCasts(e.target.id.substring(0, 7)); // 현재 state에서 가져오지 않고 바로 target에 잡힌 날것의 데이터 삽입
-        this.getMovieVideos(e.target.id.substring(0, 7));
-        this.getSimilerMovies(e.target.id.substring(0, 7));
+        getMovieCasts(e.target.id.substring(0, 7)); // 현재 state에서 가져오지 않고 바로 target에 잡힌 날것의 데이터 삽입
+        getMovieVideos(e.target.id.substring(0, 7));
+        getSimilerMovies(e.target.id.substring(0, 7));
     };
 
-    onCloseHandle = () => {
-        this.setState({ open: false });
+    const onCloseHandle = () => {
+        setOpen(false);
     };
 
-    onClickHandles = (event) => {
+    const onClickHandles = (event) => {
         let id = event.target.id;
-        id === 'btnBoxOffice'
-            ? this.setState({ viewChange: false })
-            : this.setState({ viewChange: true });
-        console.log('viewChange', this.state.viewChange);
+        id === 'btnBoxOffice' ? setViewChange(false) : setViewChange(true);
+        console.log('viewChange', viewChange);
         console.log('event:', event.target.id);
     };
 
-    printDialog = (castMember, movies, url, trailer, trailers, similer) => {
+    const printDialog = (castMember, movies, url, trailer, trailers, similer) => {
         return (
-            <Dialog open={this.state.open} onClose={this.onCloseHandle} maxWidth="md">
-                <DialogTitle>{this.state.titles}</DialogTitle>
+            <Dialog open={open} onClose={onCloseHandle} maxWidth="md">
+                <DialogTitle>{titles}</DialogTitle>
                 <DialogContent>
                     <Table>
                         <TableRow>
                             <TableCell align="center" rowSpan="4" width="25%">
-                                <img src={movies[3] ? url + movies[3] : NoImageAvailable} alt="Poster"
-                                width="200" height="300" />
+                                <img
+                                    src={movies[3] ? url + movies[3] : NoImageAvailable}
+                                    alt="Poster"
+                                    width="200"
+                                    height="300"
+                                />
                             </TableCell>
                         </TableRow>
                         <TableRow>
@@ -178,7 +172,6 @@ class TmdbList extends React.Component {
                         <TableRow>
                             {trailers.map((t) => (
                                 <TableCell>
-                                    {' '}
                                     {/*현재 어플리케이션을 돌리는 주소를 매핑 해야함(localhost) */}
                                     <ReactPlayer
                                         url={trailer + t.key + '&origin=https://localhost:3000'}
@@ -196,8 +189,12 @@ class TmdbList extends React.Component {
                             <TableBody>
                                 {similer.map((s) => (
                                     <TableCell>
-                                        <Link to={'/viewTmdb/' + s.id} >
-                                        <img src={url + s.poster_path} />
+                                        <Link to={'/viewTmdb/' + s.id}>
+                                            <img
+                                                id={s.id}
+                                                src={url + s.poster_path}
+                                                alt={url + s.poster_path}
+                                            />
                                         </Link>
                                     </TableCell>
                                 ))}
@@ -209,79 +206,78 @@ class TmdbList extends React.Component {
         );
     };
 
-    render() {
-        const { tmdbs, movies, castMember, upcommings, viewChange, trailers, similer } = this.state;
-        const { classes } = this.props
-        const url = 'https://image.tmdb.org/t/p/w200';
-        const trailer = 'https://www.youtube.com/embed/';
-        return (
-            <>
+    // render() {
+    // const { tmdbs, movies, castMember, upcommings, viewChange, trailers, similer } = this.state;
+    const { classes } = styles();
+    const url = 'https://image.tmdb.org/t/p/w200';
+    const trailer = 'https://www.youtube.com/embed/';
+    return (
+        <>
             <div align="center">
-                <button className="button" id="btnBoxOffice" onClick={this.onClickHandles}>
+                <button className="button" id="btnBoxOffice" onClick={onClickHandles}>
                     BoxOffice
                 </button>
-                <button className="button" id="btnUpcomingRelease" onClick={this.onClickHandles}>
+                <button className="button" id="btnUpcomingRelease" onClick={onClickHandles}>
                     개봉예정작
                 </button>
             </div>
-                <br />
-                {viewChange
-                    ?
-                    <div className="child">
-                    <Grid container spacing={3} align='center'>
-                    {upcommings.map((u) => (
-                        <>
-                        <Grid item xs={2}>
-                          <img
-                              className="poster"
-                              src={u.poster_path ? url + u.poster_path : NoImageAvailable}
-                              alt="img"
-                              onClick={this.onOpenChange}
-                              id={[
-                                  u.id,
-                                  u.vote_average,
-                                  u.release_date,
-                                  u.poster_path,
-                                  u.original_title,
-                                  u.overview,
-                              ]}
-                              title={u.title}
-                              width="200"
-                              height="300"
-                          />
-                          </Grid>
-                          </>
-                      ))}
+            <br />
+            {viewChange ? (
+                <div className="child">
+                    <Grid container spacing={3} align="center">
+                        {upcommings.map((u) => (
+                            <Grid item xs={2}>
+                                <img
+                                    className="poster"
+                                    src={u.poster_path ? url + u.poster_path : NoImageAvailable}
+                                    alt="img"
+                                    onClick={onOpenChange}
+                                    id={[
+                                        u.id,
+                                        u.vote_average,
+                                        u.release_date,
+                                        u.poster_path,
+                                        u.original_title,
+                                        u.overview,
+                                    ]}
+                                    title={u.title}
+                                    width="200"
+                                    height="300"
+                                />
+                            </Grid>
+                        ))}
                     </Grid>
-                    </div> 
-                    :<div className="child" >   
-                    <Grid container spacing={3} align='center'>
-                                  {tmdbs.map((m, index) => (
-                                      <>
-                                      <Grid item xs={2}>
-                                  <img
-                                  className="poster"
-                                  src={url + m.poster_path}
-                                  alt="img"
-                                  onClick={this.onOpenChange}
-                                  id={[
-                                      m.id,
-                                      m.vote_average,
-                                      m.release_date,
-                                      m.poster_path,
-                                      m.original_title,
-                                      m.overview,
-                                  ]}
-                                  title={m.title}
-                              />
-                              </Grid>
-                              </>
-                              ))}    
-                              </Grid>
-                          </div>} 
-                {this.printDialog(castMember, movies, url, trailer, trailers, similer)}
-            </>
-        );
-    }
-}
+                </div>
+            ) : (
+                <div className="child">
+                    <Grid container spacing={3} align="center">
+                        {tmdbs.map((m, index) => (
+                            <>
+                                <Grid item xs={2}>
+                                    <img
+                                        className="poster"
+                                        src={url + m.poster_path}
+                                        alt="img"
+                                        onClick={onOpenChange}
+                                        id={[
+                                            m.id,
+                                            m.vote_average,
+                                            m.release_date,
+                                            m.poster_path,
+                                            m.original_title,
+                                            m.overview,
+                                        ]}
+                                        title={m.title}
+                                    />
+                                </Grid>
+                            </>
+                        ))}
+                    </Grid>
+                </div>
+            )}
+            {printDialog(castMember, movies, url, trailer, trailers, similars)}
+        </>
+    );
+    return; // }
+};
 export default TmdbList;
